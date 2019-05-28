169342ba048bb353cea7de64d638f02d614a458c
tozokv <>
2019-05-28 12:38:15 +0200

    prebacivanje 3

--- test01/Tables/dbo.Table_2.sql
+++ test01/Tables/dbo.Table_2.sql
@@ -6,7 +6,9 @@ CREATE TABLE [dbo].[Table_2] (
 		[nova]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[novaaa]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[in]         [int] NULL,
-		[fbd]        [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[fbd]        [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[asdasd]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[asdas]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[Table_2] SET (LOCK_ESCALATION = TABLE)
