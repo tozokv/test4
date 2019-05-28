c59b375f440f0a06ec36da0c4ca40340938e6ba3
tozokv <>
2019-05-28 12:25:29 +0200

    ic3

--- test01/Tables/dbo.Table_2.sql
+++ test01/Tables/dbo.Table_2.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[Table_2] (
 		[nova]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[novaaa]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[in]         [int] NULL
+		[in]         [int] NULL,
+		[fbd]        [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[Table_2] SET (LOCK_ESCALATION = TABLE)
