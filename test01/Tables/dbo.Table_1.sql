1fe0e3ff7525371b2e52d065f849aad2f3e4b3a7
tozokv <>
2019-05-28 12:31:01 +0200

    ic

--- test01/Tables/dbo.Table_1.sql
+++ test01/Tables/dbo.Table_1.sql
@@ -6,7 +6,15 @@ CREATE TABLE [dbo].[Table_1] (
 		[Col1]     [int] NOT NULL,
 		[Col2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Col3]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[01]       [int] NULL
+		[01]       [int] NULL,
+		[fsd]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[sfd]      [int] NULL,
+		[dsa]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		CONSTRAINT [PK_Table_1]
+		PRIMARY KEY
+		CLUSTERED
+		([Col1])
+	ON [PRIMARY]
 )
 GO
 ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
