8f4846ab06d24887f388b2d02b275fa4209cb004
tozokv <>
2019-05-28 13:32:16 +0200

    ubacio kolone

--- test01/Tables/dbo.Phone_book.sql
+++ test01/Tables/dbo.Phone_book.sql
@@ -10,6 +10,11 @@ CREATE TABLE [dbo].[Phone_book] (
 		[City]             [varchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Home_phone]       [int] NULL,
 		[Mobile_phone]     [int] NULL,
+		[ads]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[adg]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[asf]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[dfhj]             [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[dfh]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [PK__Phone_bo__1788CCAC6DF29115]
 		PRIMARY KEY
 		CLUSTERED
