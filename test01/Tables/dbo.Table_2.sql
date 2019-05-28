b1c93eda9380323ac1f106cbdc57b9760dfc0f97
tozokv <>
2019-05-28 12:22:25 +0200

    ic1

--- test01/Tables/dbo.Table_2.sql
+++ test01/Tables/dbo.Table_2.sql
@@ -4,7 +4,8 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_2] (
 		[nova]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[novaaa]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[novaaa]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[in]         [int] NULL
 )
 GO
 ALTER TABLE [dbo].[Table_2] SET (LOCK_ESCALATION = TABLE)
